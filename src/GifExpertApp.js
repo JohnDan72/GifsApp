f3c32b3a9e0aba7c59671ce95b2a12e66f38aa75
JohnDan72 <mr._dany@hotmail.com>
2021-11-13 15:18:11 -0600

    Primeros avances del proyecto, se instala rsuite para las interfaces

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Divider } from 'rsuite';
+
+// #region constants
+
+// #region styled-components
+
+// #region functions
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+const GifExpertApp = () => {
+    return(
+        <>
+            <h2>Gif Expert App</h2>
+            <Divider>Divider</Divider>
+            <Button>Hello World</Button>
+        </>
+    );
+}
+
+GifExpertApp.propTypes = propTypes;
+GifExpertApp.defaultProps = defaultProps;
+
+export default GifExpertApp;
